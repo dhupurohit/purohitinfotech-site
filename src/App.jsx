be71312c7e88127c5abbe3813cd8da270df1b08b
Dhruvita Purohit <dhupurohit@gmail.com>
2025-10-02 20:53:37 +0530

    Coming soon page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./components/Home";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 class="text-3xl font-bold underline">Hello world!</h1> */}
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-[#fffbea]">
+      <div className="text-center px-6">
+        <h1 className="text-4xl md:text-5xl font-bold text-brown-800 mb-4">
+          Purohit Infotech
+        </h1>
+        <p className="text-lg md:text-xl text-brown-700 tracking-wide">
+          Coming Soon...
+        </p>
+      </div>
+    </div>
+  );
+}
+
